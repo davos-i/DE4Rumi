f13fcc15555af756ac5c8b21e8e5617a5ab3a34b
David <dave.j.innes@gmail.com>
2021-11-13 18:00:47 +1100

    Three helper functions tested working. First commit for these functions in DESEQ_helpers.

--- R/DESEQ_helpers.R
+++ R/DESEQ_helpers.R
@@ -11,18 +11,145 @@
 #' \item Column headings that match metadata
 #' \item No rownames
 #' \item First column titled gene_ensembl and contains all gene ensembl names
-#' } This also provides a print out of the dimensions and predicted treatments
+#' }This function also prints the dimensions and predicted treatments to console.
+#' @param data Dataframe of counts data to check.
+#'
 #' @export
-check_count_matrix <- function(data = cts){
-  #check column names match metadata
 
+check_count_matrix <- function(data) {
+  #check column names match metadata
+  print("**ADD colname match metadata check**")
   #check there is no rownames
-  if(tibble::has_rownames(data) == TRUE){
-    message("Rownames detected but should not be used.
-            Use tibble::rownames_to_column('gene_ensemble')
-            to correct dataframe")
+  if (tibble::has_rownames(data) == TRUE) {
+    message(
+      paste(
+        "FAIL: Rownames detected but should not be used.",
+        "Use tibble::rownames_to_column('gene_ensemble') to correct dataframe."
+      )
+    )
   } else {
-  message("Rownames not detected: PASS")
+    message("PASS: Rownames not detected")
   }
 
+  #Check if first column name is "gene_ensembl"
+  col_name_first_col <- colnames(data)[1]
+  if (col_name_first_col == "gene_ensembl") {
+    message("PASS: First column name is 'gene_ensembl'")
+  } else {
+    message(
+      paste0(
+        "FAIL: First column name is '",
+        col_name_first_col,
+        "' Should be 'gene_ensembl'"
+      )
+    )
+  }
+
+  #output dimensions
+  message(crayon::green("dimensions of full counts dataset"))
+  message(crayon::green(paste(
+    "genes: ", dim(data)[1], "    samples: ", dim(data)[2] - 1
+  )))
+
 }
+
+
+
+#'Collects human equivalent gene names and descriptions
+#'
+#'\code{annotate_gene_ensembl} creates a dataframe of human equivalent gene
+#'names and description souced from gprofiler2::gconvert.
+#'
+#'The dataframe produced by this is used for downstream annotation of files.
+#'It is also attached to the data when it is in the SummarizedExperiment class.
+#'
+#'@param data dataframe. Should be counts matrix with first
+#'column name as 'gene_ensembl'
+#'@param organism string. From gprofiler2: Organism names are constructed by
+#'concatenating the first letter of the name and the family. For sheep data:
+#'"oaries". For cattle data:"btaurus". Default is "oaries".
+#'
+#'@export
+#'
+annotate_gene_ensembl <- function(data, organism = "oaries") {
+  message(paste("Using organism:", organism))
+  converted0 <- gprofiler2::gconvert(
+    query = data$gene_ensembl,
+    organism = organism,
+    target = "ENSG",
+    # or ENSG | AFFY_BOVINE
+    filter_na = T,
+    mthreshold = 1
+  )
+
+  converted_genes <-
+    converted0 %>%
+    dplyr::select(gene_ensembl = input, gene_name = name, description)
+
+  message(
+    crayon::blue(
+      "Important - this produces duplicate gene names from annotation - however gene_ensembl remain unique.
+      This is why it's important to use gene_ensembl for all tasks and annotate output at end."
+    )
+  )
+  print(paste("duplicate genes in annotation: ",
+        nrow(converted_genes[duplicated(converted_genes$gene_name), ])))
+
+  converted_genes
+}
+
+
+
+#'Make pairwise combinations
+#'
+#'\code{make_pairwise_combinations()} creates a list of all possible unique
+#'combinations of treatment comparisons
+#'
+#'This takes the colData from the dds object, looks for a column called Region to
+#'filter out only the required top level, finds the column name defined by
+#'\code{contrast_factor} to find all unique treatment names as input to a call
+#'to \code{combn()} to get all unique pairwise comparisons.
+#'Returns a list of characters of length 2. \cr
+#'e.g. \code{[1] "LIV_HCP-HP-UMEI" "LIV_LCP-LP-UMEI"}
+#'
+#'@param coldata either a dataframe or a call to colData() on a DESeqDataSet
+#'@param contrast_factor non-string. Name of column containing treatments to contrast
+#'@param top_level_name string. Name of top level (e.g. region) to filter data
+#'with.
+#'
+#'@export
+
+make_pairwise_combinations <-
+  function(coldata,
+           contrast_factor,
+           top_level_column_name = Region,
+           top_level_filter){
+    unique_contrast_levels <-
+      coldata %>%
+      as.data.frame() %>%
+      dplyr::filter({{ top_level_column_name }} == top_level_filter) %>%
+      magrittr::extract2(contrast_factor) %>%
+      as.character() %>% #removes factors if using colData(dds) as input
+      unique()
+
+    print(paste("Contrast levels:",
+                paste(unique_contrast_levels, collapse = ", ")
+    )
+    )
+
+    utils::combn(unique_contrast_levels, 2, simplify = FALSE)  #this makes the combos
+  }
+
+
+
+
+#'Title
+#'
+#'\code{words_to_look_like_code} short description
+#'
+#'long description
+#'
+#'@param data dataframe.
+#'
+#'@export
+
